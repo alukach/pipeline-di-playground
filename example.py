6fa66c4706fa74b58dabe1143dd9523feef0ca3a
Anthony Lukach <anthonylukach@gmail.com>
2022-03-19 21:51:35 -0700

    Expand example

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
-from playground import services, providers
+from playground.services import IQueue
+from playground.providers import CloudProvider
 from playground.primitives import step, Pipeline
 
 
 class Input(BaseModel):
-    ...
+    name: str
+    age: int
 
 
 class Output(Input):
@@ -12,13 +14,31 @@ class Output(Input):
 
 
 @step(deps={"secret_1": lambda di: "foo", "secret_2": lambda di: "bar"})
-def test_step(
-    data_in: Input, queue: services.IQueue, secret_1: str, secret_2: str
-) -> Input:
-    print(f"Running with {queue=} {secret_1=} {secret_2=}")
+def demo_injection(
+    data_in: Input, queue: IQueue, secret_1: str, secret_2: str
+) -> Output:
+    print("Dependency resolutions:")
+    print(f" - {queue=}")
+    print(f" - {secret_1=}")
+    print(f" - {secret_2=}")
+    return data_in
+
+
+@step
+def make_knight(data_in: Input) -> Output:
+    data_in.name = f"Sir {data_in.name}"
+    return data_in
+
+
+@step
+def make_old(data_in: Input) -> Output:
+    data_in.age += 50
     return data_in
 
 
 if __name__ == "__main__":
-    p = Pipeline[Input, Output](env=providers.CloudProvider.local, steps=[test_step])
-    output = p.run(Input())
+    p = Pipeline[Input, Output](
+        env=CloudProvider.local, steps=[demo_injection, make_knight, make_old]
+    )
+    output = p.run(Input(name="Ringo Star", age=24))
+    print(f"{output=}")
